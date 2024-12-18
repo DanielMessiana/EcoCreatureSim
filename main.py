4b8dea5c45258bc1fabf0e468614263f104a3502
Daniel Messiana <40705754+DanielMessiana@users.noreply.github.com>
2024-12-18 14:54:46 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import numpy as np
+import random as rand
+import time, os, pygame
+
+
+class Animal():
+	genes = np.empty(1,4)
+
+	def __init__(self,)
+
+class Rabbit(Animal):
+	
+
+
+
+class Fox(Animal):
+	pass
+
+
+rabbit1 = Rabbit()
+
